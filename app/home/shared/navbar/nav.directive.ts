4a821e798fe2b260b59af1a3ba135e5df240b6b3
Lukasz Malicki <maalicki@gmail.com>
2016-12-05 18:14:20 +0100

    first commit

--- app/home/shared/navbar/nav.directive.ts
+++
@@ -1,9 +0,0 @@
-import { Directive, ElementRef, Input, Renderer } from '@angular/core';
-
-@Directive({selector: '[sidebar-menu]'})
-export class NavDirective {
-
-    constructor(el:ElementRef, renderer:Renderer) {
-        renderer.setElementStyle(el.nativeElement, 'backgroundColor', 'yellow');
-    }
-}
